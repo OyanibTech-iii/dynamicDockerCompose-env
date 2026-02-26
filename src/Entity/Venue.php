f436b990f17f53e167f6bfd63bc0c5955d001d87
pacifico <pacificooyaniv@gmail.com>
2026-02-26 13:10:27 +0800

    Add Api/ApiPlatform

--- src/Entity/Venue.php
+++ src/Entity/Venue.php
@@ -6,7 +6,9 @@ use App\Repository\VenueRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use ApiPlatform\Metadata\ApiResource;
 
+#[ApiResource]
 #[ORM\Entity(repositoryClass: VenueRepository::class)]
 class Venue
 {
