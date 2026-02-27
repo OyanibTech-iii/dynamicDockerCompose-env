bf62b35a1567b1074fcf96cd7599e3d653ed43b2
pacifico <pacificooyaniv@gmail.com>
2026-02-27 11:20:13 +0800

    fixing [] return in api

--- src/Entity/Venue.php
+++ src/Entity/Venue.php
@@ -6,9 +6,31 @@ use App\Repository\VenueRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
-use ApiPlatform\Metadata\ApiResource;
 
-#[ApiResource]
+use ApiPlatform\Metadata\ApiResource;
+use ApiPlatform\Metadata\Get;
+use ApiPlatform\Metadata\Post;
+use ApiPlatform\Metadata\Put;
+use ApiPlatform\Metadata\Delete;
+use ApiPlatform\Metadata\GetCollection;
+use Symfony\Component\Serializer\Attribute\Groups;
+
+
+#[ApiResource(
+    operations: [
+        new Get(),
+        new GetCollection(),
+        new Post(),
+        new Put(),
+        new Delete()
+    ],
+    normalizationContext: [
+        'groups' => ['venue:read']
+    ],
+    denormalizationContext: [
+        'groups' => ['venue:write']
+    ]
+)]
 #[ORM\Entity(repositoryClass: VenueRepository::class)]
 class Venue
 {
@@ -16,12 +38,13 @@ class Venue
     #[ORM\GeneratedValue]
     #[ORM\Column]
     private ?int $id = null;
-
+    #[Groups(['venue:read', 'venue:write'])]
     #[ORM\Column(length: 255)]
     private ?string $name = null;
-
+    #[Groups(['venue:read', 'venue:write'])]
     #[ORM\Column(length: 255)]
     private ?string $address = null;
+    #[Groups(['venue:read', 'venue:write'])]
 
     #[ORM\Column]
     private ?int $capacity = null;
